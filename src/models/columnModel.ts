142f7dec3b8ff2aeddf33284caca5bcdb9681f01
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2024-10-23 14:20:17 +0100

    Update columnModel.ts

--- src/models/columnModel.ts
+++ src/models/columnModel.ts
@@ -4,8 +4,6 @@ const columnSchema = new mongoose.Schema({
   columnName: { type: String, required: true }, 
   tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }], 
 });
-if (mongoose.models.Column) {
-  delete mongoose.models.Column;
-}
+
 const Column = mongoose.model("Column", columnSchema);
 export default Column;
