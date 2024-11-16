25661bbd928b17e71a2fea9007861950d9022f3c
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2024-11-16 20:01:51 +0100

    Update boardModel.ts

--- src/models/boardModel.ts
+++ src/models/boardModel.ts
@@ -4,8 +4,6 @@ const boardSchema = new mongoose.Schema({
   boardTitle: { type: String, required: true },
   columns: [{ type: mongoose.Schema.Types.ObjectId, ref: "Column" }], 
 });
-if (mongoose.models.Board) {
-  delete mongoose.models.Board;
-}
+
 const Board = mongoose.model("Board", boardSchema);
 export default Board;
