989327a63e1987225ec3273ca2b6b6f16488e0b0
Abdulrahman <110260275+Omotayo21@users.noreply.github.com>
2025-04-20 17:10:16 +0100

    Update page.tsx

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -1,103 +1,66 @@
-"use client";
+'use client'
+import React,{useState, useEffect} from "react";
+import Link from 'next/link'
 
-import { useRouter } from "next/navigation";
-import React, { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import { useAppSelector } from "../redux/hook"; // Use custom hooks
 import logodark from "../assets/logo-dark.svg";
 import logolight from "../assets/logo-light.svg";
 import Image from "next/image";
 
-const ForgotPasswordReset: React.FC = () => {
-  const { darkMode } = useAppSelector((state: any) => state.ui);
-
-  const [token, setToken] = useState<string>("");
-  const [newpassword, setNewPassword] = useState<string>("");
-  const [confirmNewPassword, setConfirmNewPassword] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string>("");
-
-  const router = useRouter();
-
-  const resetpasswordHandler = async () => {
-    if (!newpassword || !confirmNewPassword) {
-      toast.error("Passwords cannot be empty");
-      return;
-    }
-    if (newpassword !== confirmNewPassword) {
-      toast.error("Passwords do not match");
-      return;
-    }
-
+const ForgotPasswordReset:React.FC = () => {
+const { darkMode } = useAppSelector((state: any) => state.ui);
+    const router = useRouter()
+    const [email, setMail] = useState('')
+    const [loading, setLoading] = useState(false)
+const submit = async (e) => {
+    e.preventDefault
     try {
-      if (token.length > 0) {
-        setLoading(true);
-        const user = await axios.post("/api/resetpassword", {
-          token,
-          newpassword,
-        });
-        console.log(user);
-        toast.success("Password changed successfully");
-        router.push("/login");
-      }
-    } catch (error: any) {
-      toast.error(error?.response?.data?.data || "Something went wrong");
-    } finally {
-      setLoading(false);
-    }
-  };
+        setLoading(true)
+      const response = await axios.post('/api/users/forgotpassword', {email})
+        toast.success('A password Reset link has been sent to your mail')
+        router.push('/login')
+        console.log(response.data)
+    } catch (error) {
+        toast.error(error.response.data)
+        console.log(error)
+    } finally{
+        setLoading(false)
+    } 
+}
 
-  useEffect(() => {
-    const urlToken = new URLSearchParams(window.location.search).get("token");
-    setToken(urlToken || "");
-  }, []);
-
-  return (
+return(
     <>
-      {error && (
-        <div className="flex justify-center bg-gray-200 border border-green-700 w-64">
-          <h3 className="text-xl text-red-500 m-3 p-3">{error}</h3>
-        </div>
-      )}
-      <div className="flex flex-row items-center justify-center mt-20">
-        <div className="flex flex-col items-center justify-center gap-y-12 lg:w-[40rem] lg:h-[30rem] sm:w-[22rem] sm:h-[28rem] bg-white border border-black">
-          <Image
+   <div className="flex flex-row items-center justify-center mt-20">
+    <div className=" flex flex-col items-center justify-center gap-y-8 lg:w-[40rem] sm:w-[22rem] sm:h-[28rem] lg:h-[30rem] bg-white border border-black">
+        <Image
             src={darkMode ? logolight : logodark}
             alt="Logo"
             className="w-[200px] ml-5 mt-5 sm:hidden md:flex"
           />
-          <h1 className="font-bold text-3xl">
-            {loading ? "Processing" : "Reset your Password"}
-          </h1>
-          <input
-            type="password"
-            value={newpassword}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setNewPassword(e.target.value)
-            }
-            className="lg:w-96 sm:w-72 sm:h-8 rounded-md lg:h-12 border border-black pl-2"
-            placeholder="Enter your new password"
-          />
-          <input
-            type="password"
-            value={confirmNewPassword}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setConfirmNewPassword(e.target.value)
-            }
-            placeholder="Confirm your new password"
-            className="lg:w-96 sm:w-72 sm:h-8 rounded-md lg:h-12 border border-black pl-2"
-          />
-          <button
-            className="bg-indigo-500 text-white font-medium lg:w-64 lg:h-12 sm:w-48 sm:h-8 rounded-md border border-blue-500"
-            onClick={resetpasswordHandler}
-          >
-            {loading ? "Processing..." : "Reset Password"}
-          </button>
-        </div>
-      </div>
+      {loading ? (<p className="text-green-700 font-semibold">Processing , please wait </p>) : ( <> 
+       <h2 className="font-semibold text-3xl">Forgot Password</h2>
+        <p>Enter your Email adress </p>
+        <input
+        type="email"
+        value={email}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setMail(e.target.value)}
+        placeholder="Enter your email"
+        className="pl-2 lg:w-96 sm:w-72 rounded-md lg:h-12 sm:h-8 border border-black hover:border-green-600"
+       required />
+<button className="bg-indigo-500 text-white h-8 lg:w-64 sm:w-48 font-medium rounded-lg"
+onClick={submit}> Submit</button>
+  <p> Back to  <Link
+   href="/login"
+    className="font-medium text-indigo-600 hover:underline ">Login</Link></p> 
     </>
-  );
-};
+      )}
+    </div>
+    </div>
+    </>
+)
+}
 
-export default ForgotPasswordReset;
+export default ForgotPasswordReset
